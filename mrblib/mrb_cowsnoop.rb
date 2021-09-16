15b2881afcd7ec123c46af952793026de1b48c18
Uchio Kondo <udzura@udzura.jp>
2021-09-17 01:18:34 +0900

    My first snoop

--- mrblib/mrb_cowsnoop.rb
+++ mrblib/mrb_cowsnoop.rb
@@ -1,6 +1,21 @@
-class Cowsnoop
-  def bye
-    self.hello + " bye"
+def __main__(_)
+  if ARGV.empty?
+    raise "Usage: #{$0} comm"
+  end
+
+  bpf = CowsnoopBuilder.new(ARGV[0])
+  bpf.load
+  bpf.attach
+
+  interval = (ARGV[1] || 1).to_i
+
+  map = bpf.counter_map
+  map.key_type = Integer
+  map.value_type = Integer
+
+  while true
+    sleep interval
+    puts "[%s] %s" % [Time.now.to_s, map.to_h.inspect]
+    map.clear
   end
 end
-
